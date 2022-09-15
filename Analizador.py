2ca617af51c860b1d2800eb4843067d75a47b993
ChristopherMonterroso <chrismonc08@gmail.com>
2022-09-14 21:24:01 -0600

    Primera carga

---
+++ Analizador.py
@@ -0,0 +1,14 @@
+class analizador:
+    def __init__(self) -> None:
+        self.cadena = ""
+        self.linea=0
+        self.columna=0
+        self.lista=0
+
+    def compile(self):
+        archivo= open("prueba.txt","r")
+        contenido = archivo.readlines()
+        archivo.close
+        print(contenido)
+
+analizador().compile()
---
+++ Token.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+class Tokens(Enum):
+    TK_MENOR= "<"
+    TK_E_NUMERO="Numero"
+    TK_E_OPERACION="Operacion"
+    TK_MAYOR=">"
+    TK_NUMERO="[0-9]*"
+    TK_SLASH="/"
+    TK_OP_SUMA="SUMA"
+    TK_OP_RESTA="RESTA"
+    TK_OP_MULTIPLICACION="MULTIPLICACIPON"
+    TK_IGUAL="="
+    TK_E_TIPO="Tipo"
